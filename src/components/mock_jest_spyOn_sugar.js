1255b8668f19ae5b5ac0169573b551de159920ec
Darley36 <darleybest@gmail.com>
2021-10-04 19:08:01 -0500

    Mock test ended, minor changes

--- src/components/mock_jest_spyOn_sugar.js
+++ src/components/mock_jest_spyOn_sugar.js
@@ -1,18 +1,22 @@
 import * as app from "./app";
 import * as math from "./math";
 test("calls math.add", () => {
-  // store the original implementation
+  // A la variable originalAdd se asigna la funcion add del componente math
+  //el cual es el encargado de sumar
   const originalAdd = math.add;
-  // mock add with the original implementation
+  // con la propiedad jest simulamos la funcion
   math.add = jest.fn(originalAdd);
-  // spy the calls to add
+  //verificamos la funcion doAdd psando dos parametros 1 y 2
+  //y validando que la suma sea 3
   expect(app.doAdd(1, 2)).toEqual(3);
   expect(math.add).toHaveBeenCalledWith(1, 2);
-  // override the implementation
+  // Se utiliza la funcion add como implementacion y se nombra mock
+  // se compara si el metodo doAdd realiza el mismo procedimiento que la implementacion mock
   math.add.mockImplementation(() => "mock");
   expect(app.doAdd(1, 2)).toEqual("mock");
   expect(math.add).toHaveBeenCalledWith(1, 2);
-  // restore the original implementation
+  // se vuelve todo a la normalidad devolviendo la implementacion
+  // y comparando que los valores 1 y 2 sean iguales a 3
   math.add = originalAdd;
   expect(app.doAdd(1, 2)).toEqual(3);
 });
